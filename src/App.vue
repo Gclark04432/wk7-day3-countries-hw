c353b5547ec8fa8f481be7712cdd3825e37440f6
Gary Clark <gclark04432@live.co.uk>
2020-01-15 16:50:05 +0000

    initial commit with file structure

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="html">
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data: function () {
+    return {
+      countriesList: []
+    }
+  },
+  mounted: function () {
+    fetch('https://restcountries.eu/rest/v2/all')
+    .then(result => result.json())
+    .then(countries => this.countriesList = countries)
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="css" scoped>
 </style>
